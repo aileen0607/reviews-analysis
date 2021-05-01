1cc49bb1afe68ed3bd978c4959308164f5bd4ee7
aileen <s50468723@gmail.com>
2021-05-01 15:13:20 +0800

    analysis

--- r.py
+++ r.py
@@ -5,7 +5,7 @@ with open('original.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count = count + 1
-		if count %1000 == 0:
+		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完了', '總共有', len(data), '筆資料')
 
